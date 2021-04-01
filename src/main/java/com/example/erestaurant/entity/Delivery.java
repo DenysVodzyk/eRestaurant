ae3352bb79b69a5f6c7e5a253432ca93857c846c
DenysVodzyk <vodzyk@gmail.com>
2021-03-31 22:02:49 -0400

    first commit

---
+++ src/main/java/com/example/erestaurant/entity/Delivery.java
@@ -0,0 +1,16 @@
+package com.example.erestaurant.entity;
+
+import java.sql.Timestamp;
+
+public class Delivery {
+    private int id;
+    private Order order;
+    private User user;
+    private Timestamp deliveryTime;
+}
+
+
+
+
+
+
