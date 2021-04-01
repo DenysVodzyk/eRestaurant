b14c0ac1eb186d228b25f4bdd62591531ed429dd
DenysVodzyk <vodzyk@gmail.com>
2021-03-31 22:02:11 -0400

    first commit

---
+++ src/main/java/com/example/erestaurant/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.erestaurant.entity;
+
+import java.util.Date;
+
+public class User {
+    private int id;
+    private String login;
+    private String password;
+    private boolean isAuthorized;
+    private Role role;
+    private String name;
+    private String lastName;
+    private Date dOb;
+    private String email;
+    private Address address;
+    private PaymentCard paymentCard;
+    private Order order;
+}
+
+
+
+
+
