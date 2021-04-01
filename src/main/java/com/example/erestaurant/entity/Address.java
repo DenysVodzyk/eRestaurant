7857e86609329d602d8681b1f5c3f2f7189a90b1
DenysVodzyk <vodzyk@gmail.com>
2021-03-31 22:02:31 -0400

    first commit

---
+++ src/main/java/com/example/erestaurant/entity/Address.java
@@ -0,0 +1,13 @@
+package com.example.erestaurant.entity;
+
+public class Address {
+    private int id;
+    private User user;
+    private String street;
+    private String city;
+    private String province;
+    private String postalCode;
+    private String note;
+}
+
+
