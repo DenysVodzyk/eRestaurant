6c9804e2818c9608e406afcbb5cd6cba36313ce3
DenysVodzyk <vodzyk@gmail.com>
2021-03-31 22:03:21 -0400

    first commit

---
+++ src/main/java/com/example/erestaurant/entity/PaymentCard.java
@@ -0,0 +1,14 @@
+package com.example.erestaurant.entity;
+
+import java.util.Date;
+
+public class PaymentCard {
+    private int id;
+    private User user;
+    private String customerFullName;
+    private String cardNumber;
+    private int cvcCode;
+    private Date expirationDate;
+}
+
+
