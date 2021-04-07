f51b45b30c50c0225793d7350073213f2d96407a
DenysVodzyk <vodzyk@gmail.com>
2021-04-06 22:30:19 -0400

    added getters and setters

--- src/main/java/com/example/erestaurant/entity/Address.java
+++ src/main/java/com/example/erestaurant/entity/Address.java
@@ -1,13 +1,80 @@
 package com.example.erestaurant.entity;
 
+import javax.persistence.*;
+
+@Entity
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private User user;
     private String street;
     private String city;
     private String province;
     private String postalCode;
     private String note;
+
+    public Address() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", province='" + province + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", note='" + note + '\'' +
+                '}';
+    }
 }
 
 
