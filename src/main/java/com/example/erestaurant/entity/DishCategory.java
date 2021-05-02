8328fcb8a731d39ddcc21bc800ea95d499251e81
DenysVodzyk <vodzyk@gmail.com>
2021-05-01 22:31:45 -0400

    added Hiberante relationship mapping

--- src/main/java/com/example/erestaurant/entity/DishCategory.java
+++ src/main/java/com/example/erestaurant/entity/DishCategory.java
@@ -1,16 +1,27 @@
 package com.example.erestaurant.entity;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "dish_category")
 public class DishCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+
+    @OneToMany(mappedBy = "dishCategory")
+    private List<Dish> dishes;
+
+    @Column(name = "name")
     private String name;
 
     public DishCategory() {
+    }
+
+    public DishCategory(String name) {
+        this.name = name;
     }
 
     public int getId() {
@@ -19,6 +30,14 @@ public class DishCategory {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public List<Dish> getDishes() {
+        return dishes;
+    }
+
+    public void setDishes(List<Dish> dishes) {
+        this.dishes = dishes;
     }
 
     public String getName() {
@@ -33,6 +52,7 @@ public class DishCategory {
     public String toString() {
         return "DishCategory{" +
                 "id=" + id +
+                ", dishes=" + dishes +
                 ", name='" + name + '\'' +
                 '}';
     }
