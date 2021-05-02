993ae73ef97034f7ea0058fcc27b96d0bfbb31b3
DenysVodzyk <vodzyk@gmail.com>
2021-05-01 22:32:18 -0400

    added Hiberante relationship mapping

--- src/main/java/com/example/erestaurant/entity/User.java
+++ src/main/java/com/example/erestaurant/entity/User.java
@@ -1,23 +1,177 @@
 package com.example.erestaurant.entity;
 
-import java.util.Date;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
 
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+
+    @OneToMany(mappedBy = "user")
+    private List<Address> addresses;
+
+    @OneToMany(mappedBy = "user")
+    private List<PaymentCard> paymentCards;
+
+    @OneToMany(mappedBy = "user")
+    private List<CustomerOrder> orders;
+
+    @Column(name = "login")
     private String login;
+
+    @Column(name = "password")
     private String password;
-    private boolean isAuthorized;
+
+    @Column(name = "is_anonymous")
+    private boolean isAnonymous;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
     private Role role;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "last_name")
     private String lastName;
-    private Date dOb;
+
+    @Column(name = "dob")
+    private LocalDate dOb;
+
+    @Column(name = "email")
     private String email;
-    private Address address;
-    private PaymentCard paymentCard;
-    private Order order;
+
+
+    public User() {
+    }
+
+    public User(String login, String password, boolean isAnonymous, Role role, String name, String lastName, LocalDate dOb, String email) {
+        this.login = login;
+        this.password = password;
+        this.isAnonymous = isAnonymous;
+        this.role = role;
+        this.name = name;
+        this.lastName = lastName;
+        this.dOb = dOb;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    public List<PaymentCard> getPaymentCards() {
+        return paymentCards;
+    }
+
+    public void setPaymentCards(List<PaymentCard> paymentCards) {
+        this.paymentCards = paymentCards;
+    }
+
+    public List<CustomerOrder> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<CustomerOrder> orders) {
+        this.orders = orders;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAnonymous() {
+        return isAnonymous;
+    }
+
+    public void setAnonymous(boolean anonymous) {
+        isAnonymous = anonymous;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getdOb() {
+        return dOb;
+    }
+
+    public void setdOb(LocalDate dOb) {
+        this.dOb = dOb;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", addresses=" + addresses +
+                ", paymentCards=" + paymentCards +
+                ", orders=" + orders +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", isAnonymous=" + isAnonymous +
+                ", role=" + role +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", dOb=" + dOb +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
-
-
-
-
-
