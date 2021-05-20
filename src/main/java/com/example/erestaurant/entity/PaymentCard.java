5464979cc2aa82e8a9b5f5ff04505ab0a98832db
DenysVodzyk <vodzyk@gmail.com>
2021-05-19 23:20:23 -0400

    Eliminated boilerplate code with project lombok annotations

--- src/main/java/com/example/erestaurant/entity/PaymentCard.java
+++ src/main/java/com/example/erestaurant/entity/PaymentCard.java
@@ -1,8 +1,13 @@
 package com.example.erestaurant.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
 @Entity
 @Table(name = "payment_card")
 public class PaymentCard {
@@ -27,74 +32,6 @@ public class PaymentCard {
     @Column(name = "expiration_date")
     private LocalDate expirationDate;
 
-    public PaymentCard() {
-    }
-
-    public PaymentCard(String customerFullName, String cardNumber, int cvcCode, LocalDate expirationDate) {
-        this.customerFullName = customerFullName;
-        this.cardNumber = cardNumber;
-        this.cvcCode = cvcCode;
-        this.expirationDate = expirationDate;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getCustomerFullName() {
-        return customerFullName;
-    }
-
-    public void setCustomerFullName(String customerFullName) {
-        this.customerFullName = customerFullName;
-    }
-
-    public String getCardNumber() {
-        return cardNumber;
-    }
-
-    public void setCardNumber(String cardNumber) {
-        this.cardNumber = cardNumber;
-    }
-
-    public int getCvcCode() {
-        return cvcCode;
-    }
-
-    public void setCvcCode(int cvcCode) {
-        this.cvcCode = cvcCode;
-    }
-
-    public LocalDate getExpirationDate() {
-        return expirationDate;
-    }
-
-    public void setExpirationDate(LocalDate expirationDate) {
-        this.expirationDate = expirationDate;
-    }
-
-    @Override
-    public String toString() {
-        return "PaymentCard{" +
-                "id=" + id +
-                ", customerFullName='" + customerFullName + '\'' +
-                ", cardNumber='" + cardNumber + '\'' +
-                ", cvcCode=" + cvcCode +
-                ", expirationDate=" + expirationDate +
-                '}';
-    }
 }
 
 
