91fd2a1440df6a830363545d801c152674ab60be
DenysVodzyk <vodzyk@gmail.com>
2021-05-19 23:20:10 -0400

    Eliminated boilerplate code with project lombok annotations

--- src/main/java/com/example/erestaurant/entity/MenuItem.java
+++ src/main/java/com/example/erestaurant/entity/MenuItem.java
@@ -1,7 +1,12 @@
 package com.example.erestaurant.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
+@Data
+@NoArgsConstructor
 @Entity
 @Table(name = "menu_item")
 public class MenuItem {
@@ -26,75 +31,6 @@ public class MenuItem {
     @Column(name = "is_active")
     private boolean isActive;
 
-    public MenuItem() {
-    }
-
-    public MenuItem(int id, int price, boolean isKitchenMade, String ingredients, boolean isActive) {
-        this.id = id;
-        this.price = price;
-        this.isKitchenMade = isKitchenMade;
-        this.ingredients = ingredients;
-        this.isActive = isActive;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public MenuItemCategory getMenuItemCategory() {
-        return menuItemCategory;
-    }
-
-    public void setMenuItemCategory(MenuItemCategory category) {
-        this.menuItemCategory = category;
-    }
-
-    public boolean isKitchenMade() {
-        return isKitchenMade;
-    }
-
-    public void setKitchenMade(boolean kitchenMade) {
-        isKitchenMade = kitchenMade;
-    }
-
-    public String getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(String ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
-    }
-
-    @Override
-    public String toString() {
-        return "MenuItem{" +
-                "id=" + id +
-                ", price=" + price +
-                ", isKitchenMade=" + isKitchenMade +
-                ", ingredients='" + ingredients + '\'' +
-                ", isActive=" + isActive +
-                '}';
-    }
 }
 
 
