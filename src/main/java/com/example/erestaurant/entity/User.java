4b65a777de69f6272479397d37d04a98a7f9ae27
DenysVodzyk <vodzyk@gmail.com>
2021-05-19 23:20:31 -0400

    Eliminated boilerplate code with project lombok annotations

--- src/main/java/com/example/erestaurant/entity/User.java
+++ src/main/java/com/example/erestaurant/entity/User.java
@@ -1,9 +1,14 @@
 package com.example.erestaurant.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
 @Entity
 @Table(name = "user")
 public class User {
@@ -46,129 +51,4 @@ public class User {
     @Column(name = "email")
     private String email;
 
-
-    public User() {
-    }
-
-    public User(String login, String password, boolean isAnonymous, Role role, String name, String lastName, LocalDate dOb, String email) {
-        this.login = login;
-        this.password = password;
-        this.isAnonymous = isAnonymous;
-        this.role = role;
-        this.name = name;
-        this.lastName = lastName;
-        this.dOb = dOb;
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public List<Address> getAddresses() {
-        return addresses;
-    }
-
-    public void setAddresses(List<Address> addresses) {
-        this.addresses = addresses;
-    }
-
-    public List<PaymentCard> getPaymentCards() {
-        return paymentCards;
-    }
-
-    public void setPaymentCards(List<PaymentCard> paymentCards) {
-        this.paymentCards = paymentCards;
-    }
-
-    public List<CustomerOrder> getOrders() {
-        return orders;
-    }
-
-    public void setOrders(List<CustomerOrder> orders) {
-        this.orders = orders;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean isAnonymous() {
-        return isAnonymous;
-    }
-
-    public void setAnonymous(boolean anonymous) {
-        isAnonymous = anonymous;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public LocalDate getdOb() {
-        return dOb;
-    }
-
-    public void setdOb(LocalDate dOb) {
-        this.dOb = dOb;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", login='" + login + '\'' +
-                ", password='" + password + '\'' +
-                ", isAnonymous=" + isAnonymous +
-                ", role=" + role +
-                ", name='" + name + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", dOb=" + dOb +
-                ", email='" + email + '\'' +
-                '}';
-    }
 }
