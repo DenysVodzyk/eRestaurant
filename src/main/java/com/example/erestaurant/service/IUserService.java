860d4ff90a597f5565005f4b828831fb35a520d9
DenysVodzyk <vodzyk@gmail.com>
2021-05-21 22:04:08 -0400

    Added more functionality

--- src/main/java/com/example/erestaurant/service/IUserService.java
+++ src/main/java/com/example/erestaurant/service/IUserService.java
@@ -1,4 +1,30 @@
 package com.example.erestaurant.service;
 
+import com.example.erestaurant.entity.Role;
+import com.example.erestaurant.entity.User;
+import com.example.erestaurant.exception.ServiceException;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IUserService {
+    List<User> findAll();
+
+    Optional<User> findById(int id) throws ServiceException;
+
+    User findByLogin(String login) throws ServiceException;
+
+    User findByEmail(String email) throws ServiceException;
+
+    User findByNameAndLastName(String name, String lastName) throws ServiceException;
+
+    User findByIdAndPassword(int id, String password) throws ServiceException;
+
+    void addUser(User user) throws ServiceException;
+
+    public void updateUserRole(User user, Role role) throws ServiceException;
+
+    void deleteUser(User user) throws ServiceException;
+
+
 }
