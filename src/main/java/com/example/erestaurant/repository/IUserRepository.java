4c8090f05352a1a46d640803c637cac8f9f0c80e
DenysVodzyk <vodzyk@gmail.com>
2021-05-21 22:03:42 -0400

    Added few methods for finding user

--- src/main/java/com/example/erestaurant/repository/IUserRepository.java
+++ src/main/java/com/example/erestaurant/repository/IUserRepository.java
@@ -4,4 +4,17 @@ import com.example.erestaurant.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IUserRepository extends JpaRepository<User, Integer> {
+
+    User findByEmail(String email);
+
+    User findByLogin(String login);
+
+    User findByNameAndLastName(String name, String lastName);
+
+    User findByIdAndPassword(int id, String password);
+
+
 }
+
+
+
