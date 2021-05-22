72c4d42fc798ba309671674acae6a60407c95b16
DenysVodzyk <vodzyk@gmail.com>
2021-05-21 22:04:55 -0400

    Custom exception class to validate object

---
+++ src/main/java/com/example/erestaurant/exception/ObjectValidationException.java
@@ -0,0 +1,10 @@
+package com.example.erestaurant.exception;
+
+public class ObjectValidationException extends Exception {
+    public ObjectValidationException() {
+    }
+
+    public ObjectValidationException(String message) {
+        super(message);
+    }
+}
