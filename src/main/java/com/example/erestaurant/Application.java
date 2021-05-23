bb58ec17ec2a40d69083580a7f759a0fe4210e8e
DenysVodzyk <vodzyk@gmail.com>
2021-05-22 23:43:12 -0400

    Configuration for Spring Boot

--- src/main/java/com/example/erestaurant/Application.java
+++ src/main/java/com/example/erestaurant/Application.java
@@ -1,14 +1,15 @@
 package com.example.erestaurant;
 
 import com.example.erestaurant.entity.User;
+import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+//        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        SpringApplication.run(Main.class, args);
 
-        User user = new User();
 
     }
 }
