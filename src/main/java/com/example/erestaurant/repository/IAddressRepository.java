5c067d74e3d38df2f66987c2b3c9e8da47a95ec2
DenysVodzyk <vodzyk@gmail.com>
2021-05-27 00:05:09 -0400

    Added more methods to the class

--- src/main/java/com/example/erestaurant/repository/IAddressRepository.java
+++ src/main/java/com/example/erestaurant/repository/IAddressRepository.java
@@ -1,7 +1,20 @@
 package com.example.erestaurant.repository;
 
 import com.example.erestaurant.entity.Address;
+import com.example.erestaurant.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface IAddressRepository extends JpaRepository<Address, Integer> {
+
+    Address findByAddressLine1(String addressLine1);
+
+    Optional<Address> findById(int id);
+
+    List<Address> findByUserId(int id);
+
+    Address findByUserAndAddressLine1(User user, String addressLine1);
+
 }
