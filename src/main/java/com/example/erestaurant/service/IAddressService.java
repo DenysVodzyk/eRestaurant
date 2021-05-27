aaabe29dab25689b276628f3b5605c417a903fe0
DenysVodzyk <vodzyk@gmail.com>
2021-05-27 00:05:33 -0400

    Added more methods to the class

--- src/main/java/com/example/erestaurant/service/IAddressService.java
+++ src/main/java/com/example/erestaurant/service/IAddressService.java
@@ -1,4 +1,22 @@
 package com.example.erestaurant.service;
 
+import com.example.erestaurant.entity.Address;
+import com.example.erestaurant.exception.ServiceException;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IAddressService {
+
+    void add(Address address) throws ServiceException;
+
+    List<Address> getAllAddresses();
+
+    Optional<Address> getAddressById(int addressId) throws ServiceException;
+
+    List<Address> getAddressByUserId(int userId) throws ServiceException;
+
+    Address updateAddress(int oldAddressId, Address newAddress) throws ServiceException;
+
+    void deleteAddress(int addressId) throws ServiceException;
 }
