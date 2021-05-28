8643dd3d88b5db46a209e9ee08682de78d697e6c
DenysVodzyk <vodzyk@gmail.com>
2021-05-27 23:35:19 -0400

    Added annotation to fix infinite recursion due to bi-directional relation

--- src/main/java/com/example/erestaurant/entity/MenuItem.java
+++ src/main/java/com/example/erestaurant/entity/MenuItem.java
@@ -1,5 +1,6 @@
 package com.example.erestaurant.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -17,6 +18,7 @@ public class MenuItem {
 
     @ManyToOne
     @JoinColumn(name = "menu_item_category_id")
+    @JsonBackReference
     private MenuItemCategory menuItemCategory;
 
     @Column(name = "price")
