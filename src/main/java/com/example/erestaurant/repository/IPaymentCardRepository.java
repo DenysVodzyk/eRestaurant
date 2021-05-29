0267e293916e700d8c3e7805e5799c31f32068cb
DenysVodzyk <vodzyk@gmail.com>
2021-05-28 23:43:14 -0400

    Added more methods to extract information from DB

--- src/main/java/com/example/erestaurant/repository/IPaymentCardRepository.java
+++ src/main/java/com/example/erestaurant/repository/IPaymentCardRepository.java
@@ -3,5 +3,15 @@ package com.example.erestaurant.repository;
 import com.example.erestaurant.entity.PaymentCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface IPaymentCardRepository extends JpaRepository<PaymentCard, Integer> {
+
+    PaymentCard findByCustomerFullNameAndAndCardNumber(String customerFullName, String cardNumber);
+
+    Optional<PaymentCard> findById(int id);
+
+    List<PaymentCard> findByUserId(int id);
+
 }
