1e932a81786a550fafb72b865989041c3e38ac06
DenysVodzyk <vodzyk@gmail.com>
2021-05-28 23:43:57 -0400

    Added methods to the interface

--- src/main/java/com/example/erestaurant/service/IPaymentCardService.java
+++ src/main/java/com/example/erestaurant/service/IPaymentCardService.java
@@ -1,4 +1,22 @@
 package com.example.erestaurant.service;
 
+import com.example.erestaurant.entity.PaymentCard;
+import com.example.erestaurant.exception.ServiceException;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IPaymentCardService {
+    void add(PaymentCard paymentCard) throws ServiceException;
+
+    List<PaymentCard> getAllPaymentCards();
+
+    PaymentCard getPaymentCardByCustomerFullNameAndAndCardNumber(String customerFullName, String cardNumber);
+
+    Optional<PaymentCard> getPaymentCardById(int addressId) throws ServiceException;
+
+    List<PaymentCard> getPaymentCardByUserId(int userId) throws ServiceException;
+
+    void deletePaymentCard(int cardId) throws ServiceException;
+
 }
