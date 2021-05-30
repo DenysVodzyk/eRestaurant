647a90b9183959365dd77b4165e1b7e960c2cd88
DenysVodzyk <vodzyk@gmail.com>
2021-05-29 22:07:45 -0400

    Added methods related to payment

--- src/main/java/com/example/erestaurant/service/UserService.java
+++ src/main/java/com/example/erestaurant/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.erestaurant.service;
 
 import com.example.erestaurant.entity.Address;
+import com.example.erestaurant.entity.PaymentCard;
 import com.example.erestaurant.entity.Role;
 import com.example.erestaurant.entity.User;
 import com.example.erestaurant.exception.ObjectValidationException;
@@ -20,11 +21,13 @@ import java.util.Optional;
 public class UserService implements IUserService {
     private IUserRepository userRepository;
     private IAddressService addressService;
+    private IPaymentCardService paymentCardService;
 
     @Autowired
-    public UserService(IUserRepository userRepository, IAddressService addressService) {
+    public UserService(IUserRepository userRepository, IAddressService addressService, IPaymentCardService paymentCardService) {
         this.userRepository = userRepository;
         this.addressService = addressService;
+        this.paymentCardService = paymentCardService;
     }
 
     @Override
@@ -116,7 +119,6 @@ public class UserService implements IUserService {
     public void addUserAddress(int userId, Address address) throws ServiceException {
         try {
             ObjectValidator.validateObjectForNull(userId);
-            ObjectValidator.validateAddressForNull(address);
             Optional<User> user = getById(userId);
             if (user.isPresent()) {
                 address.setUser(user.get());
@@ -144,7 +146,39 @@ public class UserService implements IUserService {
             e.printStackTrace();
             throw new ServiceException("Validation for null failed.");
         }
+    }
 
+    @Override
+    public void addPaymentCard(int userId, PaymentCard paymentCard) throws ServiceException {
+        try {
+            ObjectValidator.validateObjectForNull(userId);
+            Optional<User> user = getById(userId);
+            if (user.isPresent()) {
+                paymentCard.setUser(user.get());
+                paymentCardService.add(paymentCard);
+            } else {
+                throw new ServiceException("Unable to add payment card to user with ID: " + userId + ". User is not registered in the database.");
+            }
+        } catch (ObjectValidationException e) {
+            e.printStackTrace();
+            throw new ServiceException("Validation for null failed.");
+        }
+    }
+
+    @Override
+    public List<PaymentCard> getUserPaymentCards(int userId) throws ServiceException {
+        try {
+            ObjectValidator.validateObjectForNull(userId);
+            Optional<User> user = getById(userId);
+            if (user.isPresent()) {
+                return paymentCardService.getPaymentCardByUserId(userId);
+            } else {
+                throw new ServiceException("User is not registered in the database.");
+            }
+        } catch (ObjectValidationException e) {
+            e.printStackTrace();
+            throw new ServiceException("Validation for null failed.");
+        }
     }
 
     @Override
