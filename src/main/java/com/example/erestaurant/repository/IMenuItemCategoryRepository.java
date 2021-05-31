7422e8cb68bbf092a2ebf2bee911515fed0a8e5a
DenysVodzyk <vodzyk@gmail.com>
2021-05-30 23:44:39 -0400

    Added more functionality

--- src/main/java/com/example/erestaurant/repository/IMenuItemCategoryRepository.java
+++ src/main/java/com/example/erestaurant/repository/IMenuItemCategoryRepository.java
@@ -3,5 +3,12 @@ package com.example.erestaurant.repository;
 import com.example.erestaurant.entity.MenuItemCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IMenuItemCategoryRepository extends JpaRepository<MenuItemCategory, Integer> {
+
+    Optional<MenuItemCategory> findById(int id);
+
+    MenuItemCategory findMenuItemCategoryByName(String categoryName);
+
 }
--- src/main/java/com/example/erestaurant/repository/IMenuItemRepository.java
+++ src/main/java/com/example/erestaurant/repository/IMenuItemRepository.java
@@ -3,5 +3,15 @@ package com.example.erestaurant.repository;
 import com.example.erestaurant.entity.MenuItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface IMenuItemRepository extends JpaRepository<MenuItem, Integer> {
+
+    Optional<MenuItem> findById(int id);
+
+    MenuItem findByName(String menuItemName);
+
+    List<MenuItem> findByMenuItemCategoryName(String menuItemCategoryName);
+
 }
