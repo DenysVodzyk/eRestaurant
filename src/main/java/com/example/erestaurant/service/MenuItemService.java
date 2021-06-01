31cb75493dd377ba72b5f1aa21024c2613189946
DenysVodzyk <vodzyk@gmail.com>
2021-05-31 23:24:00 -0400

    Minor change

--- src/main/java/com/example/erestaurant/service/MenuItemService.java
+++ src/main/java/com/example/erestaurant/service/MenuItemService.java
@@ -10,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.awt.*;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,7 +33,6 @@ public class MenuItemService implements IMenuItemService {
             ObjectValidator.validateMenuItemFieldsForNull(menuItem);
             if (menuItemRepository.findByName(menuItem.getName()) == null) {
                 menuItemRepository.save(menuItem);
-                menuItem.getMenuItemCategory().getMenuItems().add(menuItem);
             } else {
                 throw new ServiceException("Unable to add menu item: " + menuItem.getName() + ". It already exists in the database.");
             }
