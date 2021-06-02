d16c4693cbb6b22eac64cdb26cf18cb1a3e566af
DenysVodzyk <vodzyk@gmail.com>
2021-06-01 23:39:05 -0400

    Added more functionality

--- src/main/java/com/example/erestaurant/service/ICustomerOrderService.java
+++ src/main/java/com/example/erestaurant/service/ICustomerOrderService.java
@@ -1,4 +1,24 @@
 package com.example.erestaurant.service;
 
+import com.example.erestaurant.entity.CustomerOrder;
+import com.example.erestaurant.entity.OrderStatus;
+import com.example.erestaurant.exception.ServiceException;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ICustomerOrderService {
+
+    Optional<CustomerOrder> getById(int id) throws ServiceException;
+
+    void addOrder(CustomerOrder order) throws ServiceException;
+
+    List<CustomerOrder> getAll();
+
+    int getTotalPriceOfOrder(CustomerOrder order);
+
+    List<CustomerOrder> getByUserId(int userId);
+
+    void pushOrderStatus(int orderId, OrderStatus newOrderStatus) throws ServiceException;
+
 }
