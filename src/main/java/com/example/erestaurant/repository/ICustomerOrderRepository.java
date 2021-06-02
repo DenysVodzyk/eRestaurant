c03e00b222d111c573250e4a003d7a9c2680107f
DenysVodzyk <vodzyk@gmail.com>
2021-06-01 23:38:42 -0400

    Added methods to findById and findByUserId

--- src/main/java/com/example/erestaurant/repository/ICustomerOrderRepository.java
+++ src/main/java/com/example/erestaurant/repository/ICustomerOrderRepository.java
@@ -3,5 +3,13 @@ package com.example.erestaurant.repository;
 import com.example.erestaurant.entity.CustomerOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ICustomerOrderRepository extends JpaRepository<CustomerOrder, Integer> {
+
+    Optional<CustomerOrder> findById(int id);
+
+    List<CustomerOrder> findByUserId(int userId);
+
 }
