a991e1b9236b4233b0bf0f93ac8584fee00f1072
DenysVodzyk <vodzyk@gmail.com>
2021-06-02 22:47:52 -0400

    Added more functionality

--- src/main/java/com/example/erestaurant/service/DishService.java
+++ src/main/java/com/example/erestaurant/service/DishService.java
@@ -1,4 +1,34 @@
 package com.example.erestaurant.service;
 
-public class DishService {
+import com.example.erestaurant.entity.Dish;
+import com.example.erestaurant.entity.DishStatus;
+
+import java.util.List;
+import java.util.Optional;
+
+public class DishService implements IDishService {
+    @Override
+    public void addDish(Dish dish) {
+
+    }
+
+    @Override
+    public List<Dish> getAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<Dish> getById(int dishId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void deleteDish(int dishId) {
+
+    }
+
+    @Override
+    public void pushDishStatus(int dishId, DishStatus newDishStatus) {
+
+    }
 }
--- src/main/java/com/example/erestaurant/service/IDishService.java
+++ src/main/java/com/example/erestaurant/service/IDishService.java
@@ -1,4 +1,20 @@
 package com.example.erestaurant.service;
 
+import com.example.erestaurant.entity.Dish;
+import com.example.erestaurant.entity.DishStatus;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IDishService {
+
+    void addDish(Dish dish);
+
+    List<Dish> getAll();
+
+    Optional<Dish> getById(int dishId);
+
+    void deleteDish(int dishId);
+
+    void pushDishStatus(int dishId, DishStatus newDishStatus);
 }
