54a08a0907cc2efbfab90a0e64459d89e8e8732a
DenysVodzyk <vodzyk@gmail.com>
2021-06-02 22:47:22 -0400

    Added method findById

--- src/main/java/com/example/erestaurant/repository/IDishRepository.java
+++ src/main/java/com/example/erestaurant/repository/IDishRepository.java
@@ -3,5 +3,10 @@ package com.example.erestaurant.repository;
 import com.example.erestaurant.entity.Dish;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IDishRepository extends JpaRepository<Dish, Integer> {
+
+    Optional<Dish> findById(int id);
+
 }
